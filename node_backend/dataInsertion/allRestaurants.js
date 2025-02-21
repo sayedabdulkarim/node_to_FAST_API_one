ec87e435d5dfcb8b48c7cfa24e8c513eb5e44890
Sayed Abdul <sakarim9124@gmail.com>
2025-02-22 00:23:53 +0530

    add adminUserId to each restaurant in data insertion script

--- node_backend/dataInsertion/allRestaurants.js
+++ node_backend/dataInsertion/allRestaurants.js
@@ -4,12 +4,21 @@ import dotenv from "dotenv";
 import AllRestaurantsModal from "../modals/home/allRestaurants.js";
 import { allRestaurantsTable } from "../dummyData/allRestaurantsTable.js";
 dotenv.config();
-// Data to be inserted
+
 // Connect to MongoDB
 connectDb();
 
+// Create a default admin user ID (you should replace this with a real admin ID)
+const defaultAdminId = new mongoose.Types.ObjectId();
+
+// Add adminUserId to each restaurant
+const restaurantsWithAdmin = allRestaurantsTable.map((restaurant) => ({
+  ...restaurant,
+  adminUserId: defaultAdminId,
+}));
+
 // Insert data
-AllRestaurantsModal.insertMany(allRestaurantsTable)
+AllRestaurantsModal.insertMany(restaurantsWithAdmin)
   .then(() => {
     console.log("Data inserted successfully");
     mongoose.connection.close();
